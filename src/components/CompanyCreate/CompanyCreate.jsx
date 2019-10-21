ed8345db4e0c95584d0af7f6143601ceeca0a994
Alemakk <alexemakk@yandex.ru>
2019-10-21 18:20:12 +0300

    upd

--- src/components/CompanyCreate/CompanyCreate.jsx
+++ src/components/CompanyCreate/CompanyCreate.jsx
@@ -1,101 +1,111 @@
 import React, { useState, Fragment } from 'react'
 
-import {
-  Button
-} from 'antd'
-import {
-  AvestErrorHandling
-} from '../'
-import {
-  CompanyData
-} from './styled'
+import { Button, Typography } from 'antd'
+import { CompanyData } from './styled'
 import { decryptionCompanyData } from '../../utils'
 
 const defaultState = {
   companyData: null,
   isCreate: false
 }
+
+const { Text } = Typography
 const CompanyCreate = ({ user: { data }, createCompany }) => {
   const [state, setState] = useState({ ...defaultState })
 
-  const handleCreateCompany = () => {
+  const handleAgreeCheck = () => {
     const companyData = window.sign('123', '123')
-
     setState({
       ...state,
-      companyData: decryptionCompanyData(companyData)
+      companyData: decryptionCompanyData(companyData),
+      isCreate: true
     })
-    
+  }
+
+  const handleCreateCompany = () => {
+
   }
 
   const { companyData, isCreate } = state
   return (
     <Fragment>
       {!isCreate
-        ? <p>Проверьте ключ</p>
-        :     <CompanyData>
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+        ? <Fragment>
+          <p>Убедитесь в том, что:</p>
+          <ol>
+            <li><Text>У Вас установлен комплект абонента ГосСУОК</Text></li>
+            <li><Text>Текущий браузер MS Internet Explorer</Text></li>
+            <li><Text>Ключ ЭЦП вставлен в компьютер</Text></li>
+          </ol>
+        </Fragment>
+        : <CompanyData>
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             Дата создания
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.date}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-  
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.date}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             УНП
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.number}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-  
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.number}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             Имя компании
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.name}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-  
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.name}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             Место нахождения компании
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.city}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-  
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.city}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             Должность сотрудника
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.position}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-  
-        <CompanyData.Item>
-          <CompanyData.ItemTitle>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.position}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+
+          <CompanyData.Item>
+            <CompanyData.ItemTitle>
             Цифровой ключ
-          </CompanyData.ItemTitle>
-  
-          <CompanyData.ItemContent>
-            {companyData && companyData.key}
-          </CompanyData.ItemContent>
-        </CompanyData.Item>
-        <Button type='primary' onClick={handleCreateCompany}>Create</Button>
-      </CompanyData>
+            </CompanyData.ItemTitle>
+
+            <CompanyData.ItemContent>
+              {companyData && companyData.key}
+            </CompanyData.ItemContent>
+          </CompanyData.Item>
+        </CompanyData>
       }
+      <Button
+        type='primary'
+        onClick={isCreate ? handleCreateCompany : handleAgreeCheck}>
+        {isCreate ? 'Создать компанию' : 'Продолжить'}
+      </Button>
+      <Button type='primary' ghost>Отмена</Button>
     </Fragment>
   )
 }
