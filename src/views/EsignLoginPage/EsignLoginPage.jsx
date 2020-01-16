de8282ec2535d5386175e8e314c9e16e97dde9f1
SmileyThane <thane.crios@gmail.com>
2020-01-16 18:25:06 +0300

    fixed e-sign

--- src/views/EsignLoginPage/EsignLoginPage.jsx
+++ src/views/EsignLoginPage/EsignLoginPage.jsx
@@ -1,53 +1,28 @@
-import React, { Fragment, useEffect, useState } from 'react'
-// import axios from 'axios'
-
-import { notification, Typography } from 'antd'
+import React, { Fragment } from 'react'
+import { notification } from 'antd'
 
 import { Button } from '../../components'
-
-const {} = Typography
-
-const defaultDocumentState = {
-  isVisible: false,
-  fileLink: '',
-  fileType: '',
-  showModal: false,
-  data: [],
-  value: [],
-  fetching: false,
-  modalType: 'ecp',
-  base64files: '',
-  certs: '',
-  fileHashes: '',
-  fileData: '',
-  fileCerts: [],
-  activeFileCert: 0,
-  ecpInfo: null,
-  isSelectVisible: false,
-  isErrorWitchEcp: false,
-  declineMessage: '',
-  singleFile: null
-}
+// import axios from 'axios'
 
 const isIE = /*@cc_on!@*/false || !!document.documentMode
 
 const EsignLoginPage = props => {
-  const [documentState] = useState({ ...defaultDocumentState })
 
-  useEffect(() => {
-    if (isIE) {
-      setTimeout(() => {
-        try {
-          window.pluginLoaded()
-        } catch (error) {
-        }
-      }, 1000)
-    }
-  }, [documentState.isErrorWitchEcp])
+  if (isIE) {
+    setTimeout(() => {
+      try {
+        window.pluginLoaded()
+      } catch (error) {
+      }
+    }, 1000)
+  }
 
   const signLogin = () => {
     try {
       const sertificationObject = window.sign('111', '111')
+      notification['error']({
+        message: sertificationObject
+      })
       console.log(sertificationObject)
     } catch (error) {
       notification['error']({
