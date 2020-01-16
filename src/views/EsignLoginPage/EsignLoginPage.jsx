c856d9e86c022dfced8493fb67d2f1e3dff25ec9
SmileyThane <thane.crios@gmail.com>
2020-01-16 17:37:15 +0300

    fixed e-sign

--- src/views/EsignLoginPage/EsignLoginPage.jsx
+++ src/views/EsignLoginPage/EsignLoginPage.jsx
@@ -1,56 +1,68 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { Base64 } from 'js-base64'
-import generateHash from 'random-hash'
-import axios from 'axios'
-import { CompanyCreate } from '../../components'
-import history from '../../history.js'
-import {
-  Form,
-  Button,
-  message,
-  Typography, notification
-} from 'antd'
+// import axios from 'axios'
 
-import './EsignLoginPage.scss'
-import { decryptionCompanyData } from '../../utils'
+import { notification, Typography } from 'antd'
 
-const { Text, Title } = Typography
+import { Button } from '../../components'
 
-useEffect(() => {
-    setTimeout(() => {
-      try {
-        window.pluginLoaded()
-      } catch (error) {
-      }
-    }, 1000)
-})
+const {} = Typography
 
-
-class EsignLoginPage extends React.Component {
-
-  handleSubmit = e => {
-    e.preventDefault()
-    const companyData = window.sign('123', '123')
-    console.log(companyData)
-    //window.location.reload()
-  };
-
-  render () {
-    return (
-      <Form onSubmit={this.handleSubmit} className='form form_login' style={{ minHeight: '40rem', maxWidth: '45rem' }}>
-          <div style={{ marginTop: '1.5rem' }}>
-            <Title level={4}>Войдите с помощью ЭЦП!</Title>
-            <Text type='secondary'>
-              Вы сможете <strong>бесплатно</strong> обмениваться электронными документами с вашими контрагентами в течении <strong style={{ color: 'red' }}>90</strong> дней.
-            </Text>
-          </div>
-        <Button type='primary' htmlType='submit' className='login-form-button' style={{ marginTop: '.5rem' }}>
-          Перейти
-        </Button>
-      </Form>
-    )
-  }
+const defaultDocumentState = {
+  isVisible: false,
+  fileLink: '',
+  fileType: '',
+  showModal: false,
+  data: [],
+  value: [],
+  fetching: false,
+  modalType: 'ecp',
+  base64files: '',
+  certs: '',
+  fileHashes: '',
+  fileData: '',
+  fileCerts: [],
+  activeFileCert: 0,
+  ecpInfo: null,
+  isSelectVisible: false,
+  isErrorWitchEcp: false,
+  declineMessage: '',
+  singleFile: null
 }
 
-const WrappedLogin = Form.create({ name: 'normal_login' })(EsignLoginPage)
-export default WrappedLogin
+const EsignLoginPage = props => {
+  const [documentState] = useState({ ...defaultDocumentState })
+
+  useEffect(() => {
+    if (documentState.isErrorWitchEcp) {
+      setTimeout(() => {
+        try {
+          window.pluginLoaded()
+        } catch (error) {
+        }
+      }, 1000)
+    }
+  }, [documentState.isErrorWitchEcp])
+
+  const signLogin = () => {
+    try {
+      const sertificationObject = window.sign('111', '111')
+      console.log(sertificationObject)
+    } catch (error) {
+      notification['error']({
+        message: error.message
+      })
+    }
+  }
+  return (
+    <Fragment>
+      <Button
+        type='primary'
+        style={{ marginTop: 20 }}
+        onClick={signLogin}
+      >
+        Перейти
+      </Button>
+    </Fragment>
+  )
+}
+export default EsignLoginPage
