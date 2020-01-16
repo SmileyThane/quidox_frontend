d3b689bc1fd65b3b53432a3fba6e8be83147838d
SmileyThane <thane.crios@gmail.com>
2020-01-16 16:51:18 +0300

    fixed e-sign

--- src/views/EsignLoginPage/EsignLoginPage.jsx
+++ src/views/EsignLoginPage/EsignLoginPage.jsx
@@ -29,6 +29,21 @@ class EsignLoginPage extends React.Component {
   };
 
   render () {
+    try {
+      const isIE = /*@cc_on!@*/false || !!document.documentMode
+      if (isIE) {
+        setTimeout(() => {
+          window.pluginLoaded()
+        }, 1500)
+      }
+
+    } catch (e) {
+      notification['error']({
+        message: 'Ключ ЭЦП не найден',
+        description: 'Проверьте наличие ключа ЭЦП в USB' + e
+      })
+    }
+    const { getFieldDecorator } = this.props.form
 
     return (
       <Form onSubmit={this.handleSubmit} className='form form_login' style={{ minHeight: '40rem', maxWidth: '45rem' }}>
