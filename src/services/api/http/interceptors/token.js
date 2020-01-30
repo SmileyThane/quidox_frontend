b8769566de555b6dc3bab1a5a407e854dd1dad9a
SmileyThane <thane.crios@gmail.com>
2020-01-30 13:18:08 +0300

    minor fix

--- src/services/api/http/interceptors/token.js
+++ src/services/api/http/interceptors/token.js
@@ -1,8 +1,12 @@
 export default (instance) => {
   instance.interceptors.request.use(function (config) {
     const accessToken = 'Bearer ' + window.localStorage.getItem('authToken')
+    const sessionToken = 'Bearer ' + window.sessionStorage.getItem('authToken')
     if (accessToken) {
       config.headers.Authorization = accessToken
+      return Promise.resolve(config)
+    } else if (sessionToken) {
+      config.headers.Authorization = sessionToken
       return Promise.resolve(config)
     } else {
       return new Error('Unauthorized')
