f9c27b6e67df951d5f217bec6ce48b6fd2b542eb
SmileyThane <thane.crios@gmail.com>
2020-01-30 14:27:40 +0300

    fixed token handling

--- src/services/api/http/interceptors/token.js
+++ src/services/api/http/interceptors/token.js
@@ -1,14 +1,12 @@
 export default (instance) => {
   instance.interceptors.request.use(function (config) {
-    const accessToken = window.localStorage.getItem('authToken')
-    const sessionToken = window.sessionStorage.getItem('authToken')
-    if (accessToken !== null) {
-      console.log('accessToken')
-      config.headers.Authorization = 'Bearer ' + accessToken
-      return Promise.resolve(config)
-    } else if (sessionToken !== null) {
-      console.log('sessionToken')
-      config.headers.Authorization = 'Bearer ' + sessionToken
+    let token = window.localStorage.getItem('authToken') || window.sessionStorage.getItem('authToken')
+    // console.log(token)
+    const accessToken = 'Bearer ' + token
+    // console.log(accessToken)
+    if (accessToken) {
+      console.log('Auth attached')
+      config.headers.Authorization = accessToken
       return Promise.resolve(config)
     } else {
       return new Error('Unauthorized')
