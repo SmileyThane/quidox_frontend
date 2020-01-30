1d1e577ab4d3bf929f8ad31d3985619b2a6ae6fc
SmileyThane <thane.crios@gmail.com>
2020-01-30 14:06:44 +0300

    fixed type difference, added logging

--- src/services/api/http/interceptors/token.js
+++ src/services/api/http/interceptors/token.js
@@ -2,10 +2,12 @@ export default (instance) => {
   instance.interceptors.request.use(function (config) {
     const accessToken = window.localStorage.getItem('authToken')
     const sessionToken = window.sessionStorage.getItem('authToken')
-    if (accessToken) {
+    if (accessToken !== null) {
+      console.log('accessToken')
       config.headers.Authorization = 'Bearer ' + accessToken
       return Promise.resolve(config)
-    } else if (sessionToken) {
+    } else if (sessionToken !== null) {
+      console.log('sessionToken')
       config.headers.Authorization = 'Bearer ' + sessionToken
       return Promise.resolve(config)
     } else {
