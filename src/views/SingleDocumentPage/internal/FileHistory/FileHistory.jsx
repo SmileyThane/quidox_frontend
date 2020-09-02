f71720780d2e2a38d620193d9ceea5917af1adff
Alemakk <alexemakk@yandex.ru>
2020-09-02 22:48:34 +0300

    push

--- src/views/SingleDocumentPage/internal/FileHistory/FileHistory.jsx
+++ src/views/SingleDocumentPage/internal/FileHistory/FileHistory.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from 'antd'
+import { Typography, Tag } from 'antd'
 
 const { Text } = Typography
 
@@ -8,11 +8,22 @@ export default function ({ history }) {
   return (
     <div style={{ marginBottom: '2rem' }}>{history.map((item, idx) => (
       <div key={idx}>
+        <Text>{item.users_companies_data.user_email}<br/>[{ item.users_companies_data.company_name}]</Text>
         {item.status.map((status, i) => (
-          <div key={i}>
-            <Text>{status.status_data.name}</Text>
+          <div key={i} style={{ padding: '0.5rem', marginBottom: '0.5rem' }}>
+            <Tag color={status.status_data.color}>{status.status_data.name}</Tag><br/><br/>
+            <Text strong>Комментарий:</Text><br/>
+            <Text secondary>{status.comment && status.comment}123</Text><br/><br/>
+            {status.comment_link_basename && status.comment_link_basename !== ''
+              ? <>
+                <Text strong>Прикрепленный файл:</Text><br/>
+                <a href={status.comment_link}>status.comment_link_basename</a>
+              </>
+              : <Text strong>Ссылка отсутствует</Text>
+            }
           </div>
         ))}
+        <hr/>
       </div>
     ))}</div>
   )
