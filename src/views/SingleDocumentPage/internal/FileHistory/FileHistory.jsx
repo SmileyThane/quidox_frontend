f1410f7df6caed70e0e58280426404cbe2bb9728
SmileyThane <thane.crios@gmail.com>
2020-09-02 23:44:26 +0300

    modified file history modal window for sent messages

--- src/views/SingleDocumentPage/internal/FileHistory/FileHistory.jsx
+++ src/views/SingleDocumentPage/internal/FileHistory/FileHistory.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Tag } from 'antd'
+import { Tag, Typography } from 'antd'
 
 const { Text } = Typography
 
@@ -7,20 +7,34 @@ export default function ({ history }) {
   console.log(history)
   return (
     <div style={{ marginBottom: '2rem' }}>{history.map((item, idx) => (
+
       <div key={idx}>
-        <Text>{item.users_companies_data.user_email}<br/>[{ item.users_companies_data.company_name}]</Text>
+        <Text strong>История действий</Text>
+        <br/>
+        <Text>{item.users_companies_data.user_email}<br/>[{item.users_companies_data.company_name}]</Text>
         {item.status.map((status, i) => (
           <div key={i} style={{ padding: '0.5rem', marginBottom: '0.5rem' }}>
-            <Tag color={status.status_data.color}>{status.status_data.name}</Tag><br/><br/>
-            <Text strong>Комментарий:</Text><br/>
-            <Text secondary>{status.comment && status.comment}123</Text><br/><br/>
+              {status.status_data.id !== 1 || (status.status_data.id === 1 && item.status.length === 1)
+                ? <>
+                  <Tag color={status.status_data.color}>{status.status_data.name}</Tag><br/><br/>
+                </>
+                : <Text strong></Text>
+              }
+            {status.comment && status.comment !== ''
+              ? <>
+              <Text strong>Комментарий:</Text><br/>
+              <Text secondary>{status.comment && status.comment}</Text><br/><br/>
+              </>
+              : <Text strong></Text>
+            }
             {status.comment_link_basename && status.comment_link_basename !== ''
               ? <>
-                <Text strong>Прикрепленный файл:</Text><br/>
-                <a href={status.comment_link}>status.comment_link_basename</a>
+              <Text strong>Прикрепленный файл:</Text><br/>
+              <a href={status.comment_link}>status.comment_link_basename</a>
               </>
-              : <Text strong>Ссылка отсутствует</Text>
+              : <Text strong></Text>
             }
+
           </div>
         ))}
         <hr/>
