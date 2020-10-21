0239baf2e18589798ab950b55a6ebb9a059ce2ed
SmileyThane <thane.crios@gmail.com>
2020-10-21 10:25:22 +0300

    modified modal window with alert message

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,10 +2,9 @@ import React, { Fragment, useEffect, useState } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
 import history from '../../history'
-import { Layout, Modal} from 'antd'
-import { Button } from '../'
-import { getActiveCompany, checkActiveTariff } from '../../utils'
-import { LayoutBlock, HeaderBlock, SiderBlock, ContentBlock, FooterBlock, TariffTimePeriod } from '../'
+import { Layout, Modal } from 'antd'
+import { Button, ContentBlock, FooterBlock, HeaderBlock, LayoutBlock, SiderBlock, TariffTimePeriod } from '../'
+import { checkActiveTariff, getActiveCompany } from '../../utils'
 import { useResponseStatus } from '../../hooks'
 import { faviconIcon } from '../../resources/img'
 
@@ -16,7 +15,7 @@ const PrivateRoute = ({ component: Component, config, user: { data, isFetching }
   const [activeCompany, setActiveCompany] = useState(null)
   // eslint-disable-next-line spaced-comment
   const isIE = /*@cc_on!@*/false || !!document.documentMode
-  const authToken = window.localStorage.getItem('authToken') || window.sessionStorage.getItem('authToken');
+  const authToken = window.localStorage.getItem('authToken') || window.sessionStorage.getItem('authToken')
   useEffect(() => {
     if (authToken) {
       getUser()
@@ -66,41 +65,50 @@ const PrivateRoute = ({ component: Component, config, user: { data, isFetching }
   }
 
   return <Route {...rest}
-    render={props =>
-      (window.localStorage.getItem('authToken') || window.sessionStorage.getItem('authToken'))
-        ? <Fragment>
-          <LayoutBlock content>
-            <HeaderBlock />
-            <Layout>
-              <SiderBlock />
-              <Layout>
-                <ContentBlock>
-                  <Component {...props} />
-                </ContentBlock>
-                <TariffTimePeriod />
-                <FooterBlock />
-              </Layout>
-            </Layout>
-          </LayoutBlock>
-          {availableTariff &&
-            <Modal
-              visible
-              footer={null}
-              closable={false}
-            >
-              Внимание! Недостаточно средств на Вашем балансе и
-              мы не можем активировать очередной пакет услуг.
-              Чтобы продолжить пользование сервисом пополните, пожалуйста, баланс.
-              Кнопка - "Пополнить баланс"
-              <div style={{ marginTop: '2rem' }}>
-                <Button type='primary' onClick={handleChangeTariff}>Пополнить баланс</Button>
-                <Button type='link' onClick={handleStayOnPage}>Пополнить позже</Button>
-              </div>
-            </Modal>
-          }
-        </Fragment>
-        : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)
-    }
+                render={props =>
+                  (window.localStorage.getItem('authToken') || window.sessionStorage.getItem('authToken'))
+                    ? <Fragment>
+                      <LayoutBlock content>
+                        <HeaderBlock/>
+                        <Layout>
+                          <SiderBlock/>
+                          <Layout>
+                            <ContentBlock>
+                              <Component {...props} />
+                            </ContentBlock>
+                            <TariffTimePeriod/>
+                            <FooterBlock/>
+                          </Layout>
+                        </Layout>
+                      </LayoutBlock>
+                      {availableTariff &&
+                      <Modal
+                        visible
+                        footer={null}
+                        closable={false}
+                      >
+                        {
+                          config.data.co_brand_config ?
+                            'Внимание! Вы израсходовали ваш пакет услуг. ' +
+                            'Чтобы продолжить пользование сервисом, пожалуйста, ' +
+                            'подключите новый пакет услуг на следующие 30 дней. ' +
+                            'При нажатии на кнопку Подключить стоимость пакета ' +
+                            'с учетом НДС спишется с лицевого счета вашего абонентского номера.' :
+
+                            'Внимание! Недостаточно средств на Вашем балансе и ' +
+                            'мы не можем активировать очередной пакет услуг. ' +
+                            'Чтобы продолжить пользование сервисом пополните, пожалуйста, баланс.' +
+                            'Кнопка - "Пополнить баланс"'
+                        }
+                        <div style={{ marginTop: '2rem' }}>
+                          <Button type='primary' onClick={handleChangeTariff}>Пополнить баланс</Button>
+                          <Button type='link' onClick={handleStayOnPage}>Пополнить позже</Button>
+                        </div>
+                      </Modal>
+                      }
+                    </Fragment>
+                    : (<Redirect to={{ pathname: '/login', state: { from: props.location } }}/>)
+                }
   />
 }
 
