ce69b2570074ed1052bf017666381c0f189711c2
Hleb Prakhnitski <thane.crios@gmail.com>
2020-12-08 03:23:08 +0300

    updated tariff notification button blocks

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -34,7 +34,7 @@ const PrivateRoute = ({ component: Component, config, user: { data, isFetching }
     if (config.data.co_brand_config) {
       document.title = 'MTC SmartDoc'
       favicon.href = faviconIcon
-    }else {
+    } else {
       document.title = 'Quidox'
       favicon.href = `${process.env.REACT_APP_URL}/favicon.ico`
     }
@@ -94,14 +94,14 @@ const PrivateRoute = ({ component: Component, config, user: { data, isFetching }
                           config.data.co_brand_config ?
                             activeCompany.company_data.is_owner === true ?
 
-                            'Внимание! Вы израсходовали ваш пакет услуг. ' +
-                            'Чтобы продолжить пользование сервисом, пожалуйста, ' +
-                            'подключите новый пакет услуг на следующие 30 дней. ' +
-                            'При нажатии на кнопку "Перейти к продлению тарифа" ' +
-                            'стоимость пакета с учетом НДС спишется с лицевого счета вашего абонентского номера.' :
+                              'Внимание! Вы израсходовали ваш пакет услуг. ' +
+                              'Чтобы продолжить пользование сервисом, пожалуйста, ' +
+                              'подключите новый пакет услуг на следующие 30 дней. ' +
+                              'При нажатии на кнопку "Перейти к продлению тарифа" ' +
+                              'стоимость пакета с учетом НДС спишется с лицевого счета вашего абонентского номера.' :
                               'Внимание! Вы израсходовали ваш пакет услуг. Чтобы продолжить пользоваться сервисом, пожалуйста, ' +
                               'подключите новый пакет услуг на следующие 30 дней на номере ' + activeCompany.company_data.owner_phone_number
-                          :
+                            :
 
                             'Внимание! Недостаточно средств на Вашем балансе и ' +
                             'мы не можем активировать очередной пакет услуг. ' +
@@ -109,9 +109,19 @@ const PrivateRoute = ({ component: Component, config, user: { data, isFetching }
                             'Кнопка - "Пополнить баланс"'
                         }
                         <div style={{ marginTop: '2rem' }}>
-                          <Button type='primary' onClick={handleChangeTariff}>
-                            {config.data.co_brand_config ? 'Перейти к продлению тарифа' : 'Перейти к пополнению баланса'}
-                          </Button>
+                          {
+                            config.data.co_brand_config !== null && activeCompany.company_data.is_owner === true &&
+                            <Button type='primary' onClick={handleChangeTariff}>
+                              Перейти к продлению тарифа
+                            </Button>
+                          }
+                          {
+                            config.data.co_brand_config === null &&
+                            <Button type='primary' onClick={handleChangeTariff}>
+                              Перейти к пополнению баланса
+                            </Button>
+
+                          }
                           <Button type='link' onClick={handleStayOnPage}>
                             {config.data.co_brand_config ? 'Отмена' : 'Пополнить позже'}
                           </Button>
